48ff056d8dfa8346123746efda585f5dfa0a09d8
0xe6e6fa <zagzigzig@mail.ru>
2024-05-25 20:29:04 +0400

    add takepets page

---
+++ frontend/src/pages/takepets/_index.vue
@@ -0,0 +1,17 @@
+
+<template>
+<div>
+    <div class="h-screen w-screen bg-orange-100 flex flex-col justify-center items-center">
+        <div class=" bg-white h-3/4 w-5/6 rounded-3xl p-10 flex flex-col flex-wrap gap-10 justify-evenly items-center">
+            <div class="flex h-2/3 items-center">
+                <div class="p-4 text-xl">Мы отдаём котиков в добрые руки только по итогам СОБЕСЕДОВАНИЯ, для прохождения которого необходимо позвонить по номеру:</div>
+                <div class=" overflow-hidden">
+                    <img class="object-cover h-full w-full"
+                    src="https://i.pinimg.com/736x/92/58/c1/9258c1cd635ac8265f9b3a0acac9d8ea.jpg"></img>
+                </div>
+            </div>
+            <div class="text-5xl rounded-3xl p-2 bg-orange-100">8(937)123-45-67</div>
+        </div>
+    </div>
+</div>
+</template>
