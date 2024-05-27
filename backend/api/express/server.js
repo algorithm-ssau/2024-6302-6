f3f61d4720504943a5e1aa7ab0c980be25595b84
Nikita Boriakov <nikitaboriakov@yandex.ru>
2024-05-27 22:14:27 +0300

    Initial commit
    Основы для работы с данными о животных в базе данных MongoDB с использованием Express.js и Mongoose.

--- backend/api/express/server.js
+++ backend/api/express/server.js
@@ -1,10 +1,11 @@
+// Начало работы с back 
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
+// Подключение к базе данных MongoDB - const db - управление соединением
 mongoose.connect("mongodb://localhost/animalshelter");
 const db = mongoose.connection;
-
+// Определение структуры документов 
 const AnimalSchema = new mongoose.Schema({
   name: String,
   age: Number,
@@ -14,30 +15,33 @@ const AnimalSchema = new mongoose.Schema({
 });
 
 const Animal = mongoose.model("Animal", AnimalSchema);
-
+// Импорт express
 const app = express();
+// Обработка JSON и URL-кодированных данных
 app.use(express.json());
 app.use(express.text());
+// Cross-Origin Resource Sharing
 app.use(cors());
 
+// Запуск сервера на порту 3000
 app.listen(3000);
-
+// получени всех
 app.get("/animals", async (req, res) => {
   const animals = await Animal.find();
   res.json(animals);
 });
-
+// получение по ID
 app.get("/animals/:id", async (req, res) => {
   const animal = await Animal.findById(req.params.id);
   res.json(animal);
 });
-
+// создание
 app.post("/animals", async (req, res) => {
   const animal = new Animal(JSON.parse(req.body));
   await animal.save();
   res.send("ok");
 });
-
+// обновление
 app.put("/animals/:id", (req, res) => {});
-
+// удаление 
 app.delete("/animals/:id", (req, res) => {});
