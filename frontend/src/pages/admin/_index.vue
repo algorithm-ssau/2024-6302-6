0bc1e2499c542390659c86369ce696d5b8861dab
IvanovVladislav03 <3ripple6ix@gmail.com>
2024-05-30 20:43:20 +0400

    Добавил добавление продуктов через админскую панель

--- frontend/src/pages/admin/_index.vue
+++ frontend/src/pages/admin/_index.vue
@@ -27,6 +27,49 @@ async function add() {
     console.log(error);
   }
 }
+
+
+
+
+/* ПРОДУКТ */
+import { ref } from 'vue';
+
+const name_product = ref("");
+const price_product = ref("");
+const image_product = ref("");
+
+const URL_prod = "http://localhost:5000";
+
+async function addProduct() {
+  try {
+    const response = await fetch(URL_prod + "/api/products", {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name_product.value,
+        price: parseFloat(price_product.value),
+        image: image_product.value
+      }),
+    });
+
+    if (response.ok) {
+      alert("Продукт успешно добавлен!");
+      // Очистка полей ввода после успешного добавления продукта
+      name_product.value = "";
+      price_product.value = "";
+      image_product.value = "";
+    } else {
+      console.error("Ошибка при добавлении продукта:", response.statusText);
+      alert("Ошибка при добавлении продукта.");
+    }
+  } catch (error) {
+    console.error("Ошибка при добавлении продукта:", error);
+    alert("Ошибка при добавлении продукта.");
+  }
+}
 </script>
 
 <template>
@@ -48,5 +91,16 @@ async function add() {
       class="border"
     />
     <button @click="add">Добавить</button>
+  </div>
+
+    
+
+  <! -- ПРОДУКТЫ -->
+  <div class="admin-panel flex flex-col p-4"> 
+    <h2 class="text-center mb-4">Продукт</h2>
+    <input type="text" placeholder="Название продукта" v-model="name_product" class="border p-2 mb-2" />
+    <input type="text" placeholder="Цена продукта" v-model="price_product" class="border p-2 mb-2" />
+    <input type="text" placeholder="URL изображения продукта" v-model="image_product" class="border p-2 mb-2" />
+    <button @click="addProduct" class="bg-blue-500 text-white p-2 mt-2 rounded">Добавить продукт</button>
   </div>
 </template>
