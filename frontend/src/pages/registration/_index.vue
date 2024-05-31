951b478d2c0b8efff3f4265876d958acf5aeea9c
Nikita Boriakov <nikitaboriakov@yandex.ru>
2024-05-31 16:29:37 +0300

    Init value in MongoDB

--- frontend/src/pages/registration/_index.vue
+++ frontend/src/pages/registration/_index.vue
@@ -1,9 +1,12 @@
 <script setup>
 import Header from "../../components/Header.vue";
-const username = defineModel("username");
+import { ref } from "vue";
+    const username = defineModel("username");
     const email = defineModel("email");
     const password = defineModel("password");
+    const usersystem = defineModel("usersystem");
     const URL = "http://localhost:3000";
+    const persons = ref([]);
 
     async function add() 
     {
@@ -18,12 +21,36 @@ const username = defineModel("username");
             username: username.value,
             email: email.value,
             password: password.value,
+            usersystem: usersystem.value
           }),
         });
       } catch (error) {
         console.log(error);
       }
     }
+
+    async function get_2() 
+    {
+      const response = await fetch(URL + "/users/:name");
+      persons.value = await response.json();
+      console.log("Вывод результата в консоль: ");
+      console.log(persons.value);
+    }
+
+    // async function get() 
+    // {
+    //   const response = await fetch(URL + "/users", {
+    //       method: "GET",
+    //       headers: { 
+    //         "Accept": "application/json"
+    //       }
+    //   });
+    //   if (response.ok === true) {
+    //       const user = await response.json();
+    //       console.log(user);
+    //   }
+    // }
+
 </script>
 <!-- template - username - email - password -->
   <template>
@@ -40,6 +67,7 @@ const username = defineModel("username");
         <label for="password">Пароль:</label>
         <input type="text" id="password" v-model="password" required>
         <button @click="add">Зарегистрироваться</button>
+        <button @click="get_2">Получить</button>
       </form>
     </div>
   </template>
